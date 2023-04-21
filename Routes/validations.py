18b6bd88515cec7f81fc8248ef2c32e49991ae03
chelavik <106871646+chelavik@users.noreply.github.com>
2023-04-21 15:32:12 +0400

    Update validations.py

--- Routes/validations.py
+++ Routes/validations.py
@@ -43,12 +43,12 @@ async def get_hundred_physics(token: Token, page_id: int):
         raise HTTPException(status_code=401, detail='bad token')
 
 
-@router.post('/get_all_validations', tags=['business'])
+@router.post('/get_all_validations/{page_id}', tags=['business'])
 async def get_all_validations(token: Token, page_id: int):
     try:
         username, user_type = unpack_token(token.access_token)
         if user_type == "business":
-            info = await SQLDatabase.get_all_validations(username)
+            info = await SQLDatabase.get_all_validations(username, page_id)
             for dictionary in info:
                 dictionary['validation_date'] = str(dictionary['validation_date'])
             return JSONResponse(info)
