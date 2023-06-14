9b142133edc10ae58cffac01df7de019a0728c33
chelavik <asdqwehamidullin@yandex.ru>
2023-06-14 21:32:56 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-host = 'localhost'  # Local IP address
+host = 'localhost'
 port = 3306  # Local port
-
 
 user_users = 'admin_backend_test'  # MySQL server username
 users_password = 'w65S?nGC'  # MySQL server password
 db_users = 'waterdroplet_model'  # MySQL database name
-
 
 user_trans = 'admin_backend_trans_test'
 trans_password = 'ZS7bq|#%'
@@ -13,4 +11,5 @@ db_trans = 'transactions'  # MySQL database name
 
 VALIDATION_CONST = 100
 
-SECRET_KEY = '$2b$12$1smi1wC2W.tNOY5Tuu4shumq3Mi65kFH4bW296AT1NfeKqGxFRSqi'
+SECRET_KEY = '$2b$12$1smi1wC2W.tNOY5Tuu4shumq3Mi65kFH4bW296AT1NfeKqGxFRSqi'  # thisissupersecret
+ENCRYPTION_KEY = b'5SoLsR3_tMsKkk2AGODf8CqkECiI5g6uezHuWC-kgwY='
