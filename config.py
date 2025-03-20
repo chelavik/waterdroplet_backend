d3ca39eccfa95eb4dd57cbf304964f0e3773a1ed
chelavik <106871646+chelavik@users.noreply.github.com>
2025-03-20 15:54:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-host = '185.185.70.161'
+# host = 
 port = 3306  # Local port
 
-user_users = 'admin_backend'  # MySQL server username
-users_password = 'w65S?nGC'  # MySQL server password
-db_users = 'waterdroplet_model'  # MySQL database name
+# user_users = MySQL server username
+# users_password = # MySQL server password
+# db_users =  # MySQL users database name
 
-user_trans = 'admin_backend_trans'
-trans_password = 'ZS7bq|#%'
-db_trans = 'transactions'  # MySQL database name
+# user_trans = transaction_db_user
+# trans_password = transaction_db_password
+# db_trans = MySQL transactions database name
 
 VALIDATION_CONST = 100
 
 SECRET_KEY = '$2b$12$1smi1wC2W.tNOY5Tuu4shumq3Mi65kFH4bW296AT1NfeKqGxFRSqi'  # thisissupersecret
 ENCRYPTION_KEY = b'5SoLsR3_tMsKkk2AGODf8CqkECiI5g6uezHuWC-kgwY='
 
-iputoken = 'apitokentest'
+# iputoken = ipu token validation
